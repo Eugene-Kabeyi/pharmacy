dcb2a47ba7fb4338340b02ca2bd6e24f4c28affa
Eugene Kabeyi <104126948+Eugene-Kabeyi@users.noreply.github.com>
2024-11-26 08:49:30 +0300

    Delete codes.sql

--- codes.sql
+++
@@ -1,5 +0,0 @@
-SELECT DOC.Name AS Doctor_Name, DOC.Specialization AS Specialization , PRES.Drug_ID, DR.Name AS  Drug_Name, CUSTOMER.Name AS Customer_Name, DM.Name AS Manufacturer_Name
-FROM
-PRESCRIBES PRES
-JOIN
-DOCTOR DOC ON PRES
